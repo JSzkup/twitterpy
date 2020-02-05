c97fcaf80b48db2bd86a4049de4899ddb14ece03
Szkup <jszkup@COUNTY.SUF>
2020-02-05 11:15:37 -0500

    tweet object, functions split for readability

--- main.py
+++ main.py
@@ -20,9 +20,12 @@ from bs4 import BeautifulSoup as bs
 import time
 import datetime
 
-# organizing info
-import numpy
-import pandas
+### organizing info
+##import numpy
+##import pandas
+
+# regular expressions to parse text
+import re
 
 def init_driver():
     #Chrome 79
@@ -56,8 +59,6 @@ class WaitForMoreThanNElementsToBePresent(object):
 
 def search_twitter(driver, keywords):
  
-    # wait until the search box has loaded to search within it
-    wait = WebDriverWait(driver, 10)
     box = driver.wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"react-root\"]/div/div/div[2]/header/div[2]/div[1]/div[1]/div/div[2]/div/div/div/form/div[1]/div/div/div[2]/input")))
     driver.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div[2]/header/div[2]/div[1]/div[1]/div/div[2]/div/div/div/form/div[1]/div/div/div[2]/input").clear()
     # your typed keywords is typed in
@@ -67,9 +68,14 @@ def search_twitter(driver, keywords):
     # initial wait for the search results to load
     driver.implicitly_wait(1)
  
+    # clicks "Latest" tab to get most recent tweets
     driver.find_element_by_link_text("Latest").click()
     driver.wait = WebDriverWait(driver, 1) 
 
+    return
+
+def pull_tweets(driver):
+    wait = WebDriverWait(driver, 10)
 
     try:
         # wait until the first search result is found. Search results will be tweets, which are html list items and have the class='data-item-id'
@@ -102,75 +108,48 @@ def search_twitter(driver, keywords):
                 # if no more are visible the "wait.until" call will timeout. Catch the exception and exit the while loop
                 break
  
-        # extract the html for the whole lot
-        pageSource = driver.page_source
- 
     except TimeoutException:
  
         # if there are no search results then the "wait.until" call in the first "try" statement will never happen and it will time out. So we catch that exception and return no html
-        pageSource=None
- 
-    return pageSource
-
-def extract_tweets(pageSource):
- 
-    soup = bs(pageSource,'lxml')
- 
-    tweets = []
-    for li in soup.find_all("li", class_='js-stream-item'):
- 
-        # If our li doesn't have a tweet-id, we skip it as it's not going to be a tweet.
-        if 'data-item-id' not in li.attrs:
-            continue
- 
-        else:
-            tweet = {
-                'tweet_id': li['data-item-id'],
-                'text': None,
-                'user_id': None,
-                'user_screen_name': None,
-                'user_name': None,
-                'created_at': None,
-                'retweets': 0,
-                'likes': 0,
-                'replies': 0
-            }
- 
-            # Tweet Text
-            text_p = li.find("p", class_="tweet-text")
-            if text_p is not None:
-                tweet['text'] = text_p.get_text()
- 
-            # Tweet User ID, User Screen Name, User Name
-            user_details_div = li.find("div", class_="tweet")
-            if user_details_div is not None:
-                tweet['user_id'] = user_details_div['data-user-id']
-                tweet['user_screen_name'] = user_details_div['data-screen-name']
-                tweet['user_name'] = user_details_div['data-name']
- 
-            # Tweet date
-            date_span = li.find("span", class_="_timestamp")
-            if date_span is not None:
-                tweet['created_at'] = float(date_span['data-time-ms'])
- 
-            # Tweet Retweets
-            retweet_span = li.select("span.ProfileTweet-action--retweet > span.ProfileTweet-actionCount")
-            if retweet_span is not None and len(retweet_span) > 0:
-                tweet['retweets'] = int(retweet_span[0]['data-tweet-stat-count'])
- 
-            # Tweet Likes
-            like_span = li.select("span.ProfileTweet-action--favorite > span.ProfileTweet-actionCount")
-            if like_span is not None and len(like_span) > 0:
-                tweet['likes'] = int(like_span[0]['data-tweet-stat-count'])
- 
-            # Tweet Replies
-            reply_span = li.select("span.ProfileTweet-action--reply > span.ProfileTweet-actionCount")
-            if reply_span is not None and len(reply_span) > 0:
-                tweet['replies'] = int(reply_span[0]['data-tweet-stat-count'])
- 
-            tweets.append(tweet)
+        tweets = None
  
     return tweets
+
+class Tweet(object):
+    def __init__(self, tweet_id, tweet_name, tweet_handle, text, comments, retweets, likes):
+        self.tweet_id = ""
+        self.tweet_name = ""
+        self.tweet_handle = ""
+        self.text = ""
+        self.comments = 0
+        self.retweets = 0
+        self.likes = 0
+
+
+def parse_tweets(tweets):
+    
+    parsedText = []
+
+    #for i in tweets:
+    #    text = i.text
+ 
+
+
+
+
+        ##tweet = {
+        ##    'tweet_id': li['data-item-id'],
+        ##    'text': None,
+        ##    'user_id': None,
+        ##    'user_screen_name': None,
+        ##    'user_name': None,
+        ##    'created_at': None,
+        ##    'retweets': 0,
+        ##    'likes': 0,
+        ##    'replies': 0
+        ##}
+ 
+    return parsedText
 
 
 def close_driver(driver):
@@ -187,12 +166,14 @@ if __name__ == "__main__":
     # What is written in the twitter searchbar 
     # TODO look into advanced search
     keywords = "Suffolk County"
-    pageSource = search_twitter(driver, keywords)
+    search_twitter(driver, keywords)
+
+    tweets = pull_tweets(driver)
  
     # extract info from the search results
-    tweets = extract_tweets(pageSource)
+    finalTweets = parse_tweets(tweets)
 
- 
+
     # close the driver:
     close_driver(driver)
 
