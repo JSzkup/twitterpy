0b37f529794343e1cf6fcad88a7197a98b3e37fc
Jonathan Szkup <jszkup101@gmail.com>
2020-03-16 09:31:00 -0400

    fixed path, deprecation warning

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def init_driver():
     chrome_options.add_argument("--headless")
 
     driver = webdriver.Chrome(
-        executable_path=r'C:\Code\Python\twitterpy\chromedriver.exe', options=chrome_options)
+        executable_path=r'C:\PythonFiles\TwitterScraper\chromedriver.exe', options=chrome_options)
 
     return driver
 
@@ -54,7 +54,7 @@ def init_regex():
         "username": re.compile(r'(?P<username>@[a-zA-Z_0-9]{,15})'),
         # TODO works with (?s) for no apparent reason, gives a deprecation warning, tweets are supposed to only be 280 chars +- handles/usernames (but not hashtags)
         # TODO hacky nonetext fix by increading character count to 1000 in text
-        "text": re.compile(r'((?P<before>(\d(s|m|h|d))|(>@[a-zA-Z_0-9]{,15})|(and \d others))(?P<text>(?s).{,1000}))')
+        "text": re.compile(r'((?P<before>(\d(s|m|h|d))|(>@[a-zA-Z_0-9]{,15})|(and \d others))(?P<text>.{,1000}))', re.DOTALL)
     }
     # https://stackoverflow.com/questions/41805522/can-a-python-dictionary-use-re-compile-as-a-key
 
